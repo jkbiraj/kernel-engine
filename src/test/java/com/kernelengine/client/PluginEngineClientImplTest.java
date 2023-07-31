44b4f0b37966cfa5805730aafd3d4410197b5e80
jkbiraj <83824983+jkbiraj@users.noreply.github.com>
2023-07-31 11:24:12 +0530

    Update PluginEngineClientImplTest.java

--- src/test/java/com/kernelengine/client/PluginEngineClientImplTest.java
+++ src/test/java/com/kernelengine/client/PluginEngineClientImplTest.java
@@ -1,17 +1,9 @@
 package com.kernelengine.client;
 
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class PluginEngineClientImplTest {
 
     @Test
-    private void testFetchFirewallData() throws IOException {
-        PluginEngineClientImpl pluginEngineClient = new PluginEngineClientImpl();
-        pluginEngineClient.fetchFirewallData();
+    private void testFetchFirewallData() throws IOException, FetchDataException {
 
     }
 }
